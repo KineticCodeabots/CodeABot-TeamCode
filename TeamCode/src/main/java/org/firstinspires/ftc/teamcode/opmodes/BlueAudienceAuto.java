366b2710bfd72392cf208b35dc9a3f3b70de1649
FTC_CodeABot <codeabot.ftc@gmail.com>
2023-12-04 16:20:28 +0200

    2023/12/04 ftc meeting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueAudienceAuto.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.CodeabotCommon;
+
+@Autonomous(name="Blue/Audience", group="blue")
+public class BlueAudienceAuto extends AutoBase {
+    private final CodeabotCommon.Alliance alliance = CodeabotCommon.Alliance.BLUE;
+    private final CodeabotCommon.StartingLocation startingLocation = CodeabotCommon.StartingLocation.AUDIENCE;
+}
