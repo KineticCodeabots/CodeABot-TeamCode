2970d0fe41bc96607ad3660732d4f62c991db71e
bluescorpian <dsharryh27@gmail.com>
2023-12-15 16:00:00 +0200

    15/12/23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoBase.java
@@ -5,16 +5,35 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.CodeabotCommon;
 
 public abstract class AutoBase extends LinearOpMode {
-    private final CodeabotCommon.Alliance alliance = null;
-    private final CodeabotCommon.StartingLocation startingLocation = null;
+    private final double DRIVE_SPEED = 0.7;
+    private final double TURN_SPEED = 0.2;
+
+    private final CodeabotCommon.Alliance alliance = CodeabotCommon.Alliance.BLUE;
+    private final CodeabotCommon.StartingLocation startingLocation = CodeabotCommon.StartingLocation.AUDIENCE;
 
     private Robot robot = new Robot(this);
 
     @Override
     public void runOpMode() {
         robot.init(true);
-
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+        waitForStart();
+        robot.startAuto();
+        robot.driveStraight(DRIVE_SPEED, 33, 0);
+        alianceTurnToHeading(TURN_SPEED, -90);
+        if (startingLocation == CodeabotCommon.StartingLocation.AUDIENCE) {
+            robot.driveStraight(DRIVE_SPEED, 80, -90);
+        }
+        alianceTurnToHeading(TURN_SPEED, -180);
+        robot.driveStraight(DRIVE_SPEED, 20, -180);
+    }
+
+    private void alianceTurnToHeading(double maxTurnSpeed, double heading) {
+        if (alliance == CodeabotCommon.Alliance.BLUE) {
+            robot.turnToHeading(maxTurnSpeed, heading);
+        } else {
+            robot.turnToHeading(maxTurnSpeed, -heading);
+        }
     }
 }
