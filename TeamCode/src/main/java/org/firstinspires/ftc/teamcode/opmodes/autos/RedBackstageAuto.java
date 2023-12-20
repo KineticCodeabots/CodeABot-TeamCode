5e3345925d9729037d619a782072566625a37ad9
Harry Kruger <dsharryh27@gmail.com>
2023-12-20 15:14:28 +0200

    fix auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/RedBackstageAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/RedBackstageAuto.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.CodeabotCommon;
 import org.firstinspires.ftc.teamcode.opmodes.AutoBase;
 
-@Autonomous(name = "Red/Audience", group = "red")
+@Autonomous(name = "Red/Backstage", group = "red")
 public class RedBackstageAuto extends AutoBase {
     private final CodeabotCommon.Alliance alliance = CodeabotCommon.Alliance.RED;
-    private final CodeabotCommon.StartingLocation startingLocation = CodeabotCommon.StartingLocation.AUDIENCE;
+    private final CodeabotCommon.StartingLocation startingLocation = CodeabotCommon.StartingLocation.BACKSTAGE;
 }
