01ec77262281a649c0cdaa782cfe90d814fdb975
Harry Kruger <dsharryh27@gmail.com>
2024-01-18 17:05:40 +0200

    Add slowdown and some tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmController.java
@@ -11,26 +11,16 @@ public class ArmController {
     private DcMotorEx motor = null;
     private final int MAX_VELOCITY = 600;
     private final double MAX_POWER = 0.3; // For safety reasons
-    private final int VIRTUAL_STOP_POSITION = 90;
+    private final int VIRTUAL_STOP_POSITION = 105;
     private final int VIRTUAL_STOP_THRESSHOLD = 10;
+    private final int VIRTUAL_STOP_SLOWDOWN = 50;
+    private final int VIRTUAL_STOP_SLOWDOWN_VELOCITY = 300;
 
-    private final ArmPIDF velocityPID = new ArmPIDF(0.0005, 0.0000, 0.0001, 0.0005);
+    private final PIDF velocityPID = new PIDF(0.0005, 0.0000, 0.0005, 0.0005);
     private final PID positionPID = new PID(0.05, 0.001, 0);
 
     private int previousPosition = 0;
     private boolean previousZeroing = false;
-
-    static class ArmPIDF extends PIDF {
-        public ArmPIDF(double Kp, double Ki, double Kd, double Kv) {
-            super(Kp, Ki, Kd, Kv);
-        }
-
-        @Override
-        public double update(double reference, double state) {
-            double powerCommand = super.update(reference, state);
-            return (reference < 0) ? Math.min(powerCommand, 0) : Math.max(powerCommand, 0);
-        }
-    }
 
     public ArmController(DcMotorEx motor) {
         this.motor = motor;
@@ -54,7 +44,15 @@ public class ArmController {
             if (pushDown && power == 0) {
                 if (motor.getCurrentPosition() > 20) setPower(-0.1);
                 else setPower(0);
-            } else setPower(velocityPID.update(power * MAX_VELOCITY, motor.getVelocity()));
+            } else {
+                boolean inSlowdown = motor.getCurrentPosition() < VIRTUAL_STOP_POSITION + VIRTUAL_STOP_SLOWDOWN;
+                double reference = power * MAX_VELOCITY;
+                if (inSlowdown) reference = Math.min(reference, VIRTUAL_STOP_SLOWDOWN_VELOCITY);
+                double powerCommand = velocityPID.update(reference, motor.getVelocity());
+                if (!inSlowdown)
+                    powerCommand = (reference < 0) ? Math.min(powerCommand, 0) : Math.max(powerCommand, 0);
+                setPower(powerCommand);
+            }
             positionPID.reset();
         } else {
             motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMode.java
@@ -123,10 +123,10 @@ public class TeleOpMode extends LinearOpMode {
                 currentGamepad2.left_bumper,
                 currentGamepad2.right_bumper);
 
-        if (currentGamepad2.dpad_up && !previousGamepad2.dpad_up) {
-            robot.setHandPosition(Range.clip(robot.handPosition + 0.05, 0, 1));
-        } else if (currentGamepad2.dpad_down && !previousGamepad2.dpad_down) {
-            robot.setHandPosition(Range.clip(robot.handPosition - 0.05, 0, 1));
+        if (currentGamepad2.dpad_up) {
+            robot.setHandPosition(Range.clip(robot.handPosition + 0.001, 0, 1));
+        } else if (currentGamepad2.dpad_down) {
+            robot.setHandPosition(Range.clip(robot.handPosition - 0.001, 0, 1));
         }
         // Change hand position using A, B, and Y buttons
         if (currentGamepad2.a && !previousGamepad2.a) {
