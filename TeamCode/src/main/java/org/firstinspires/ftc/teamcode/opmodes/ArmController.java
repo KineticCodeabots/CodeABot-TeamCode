508329979018768debbb208901efd7ca3907c6ab
Harry Kruger <dsharryh27@gmail.com>
2024-01-19 09:56:17 +0200

    virtual stop improvements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmController.java
@@ -11,15 +11,15 @@ public class ArmController {
     private DcMotorEx motor = null;
     private final int TARGET_VELOCITY = 600;
     private final double MAX_POWER = 0.3; // For safety reasons
-    private final int VIRTUAL_STOP_POSITION = 90;
-    private final int VIRTUAL_STOP_THRESSHOLD = 10;
+    private final int VIRTUAL_STOP_POSITION = 80;
+    private final int VIRTUAL_STOP_THRESSHOLD = 5;
     private final int VIRTUAL_STOP_SLOWDOWN = 100;
     private final int VIRTUAL_STOP_SLOWDOWN_VELOCITY = 250;
 
     private final PIDF velocityPIDF = new PIDF(0.0005, 0.0000, 0.000, 0.00045);
     private final PID positionPID = new PID(0.05, 0.002, 0);
 
-    private int previousPosition = 0;
+    private boolean onVirtualStop = false;
     private boolean previousZeroing = false;
 
     public ArmController(DcMotorEx motor) {
@@ -34,14 +34,15 @@ public class ArmController {
     }
 
     public void update(double power, boolean pushDown, boolean zeroing) {
-        if (motor.getCurrentPosition() > VIRTUAL_STOP_POSITION + VIRTUAL_STOP_THRESSHOLD || power > 0 || zeroing || pushDown) {
+        if ((motor.getCurrentPosition() > VIRTUAL_STOP_POSITION + 10 && !onVirtualStop) || power > 0 || pushDown) {
+            onVirtualStop = false;
             boolean inSlowdown = motor.getCurrentPosition() < VIRTUAL_STOP_POSITION + VIRTUAL_STOP_SLOWDOWN;
-            if (pushDown) {
+            if (pushDown || (power != 0 && !inSlowdown)) {
                 motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
             } else {
                 motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             }
-            
+
             if (pushDown && motor.getCurrentPosition() < 20) {
                 setPower(0);
             } else {
@@ -57,8 +58,9 @@ public class ArmController {
 
             positionPID.reset();
         } else {
+            onVirtualStop = true;
             motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            if (motor.getCurrentPosition() < VIRTUAL_STOP_POSITION)
+            if (Math.abs(motor.getCurrentPosition() - VIRTUAL_STOP_POSITION) > VIRTUAL_STOP_THRESSHOLD)
                 setPower(positionPID.update(VIRTUAL_STOP_POSITION, motor.getCurrentPosition()));
             else {
                 setPower(0);
@@ -68,7 +70,6 @@ public class ArmController {
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         }
-        previousPosition = motor.getCurrentPosition();
         previousZeroing = zeroing;
     }
 
