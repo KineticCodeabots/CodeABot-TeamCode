2230a3fcec3f62c54cab6899b814d3156a2dc402
Harry Kruger <dsharryh27@gmail.com>
2024-01-19 09:17:40 +0200

    simplify PIDF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF.java
@@ -5,19 +5,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class PIDF extends PID {
     private double Kv;
 
-    ElapsedTime timer = new ElapsedTime();
-
     public PIDF(double Kp, double Ki, double Kd, double Kv) {
         super(Kp, Ki, Kd);
         this.Kv = Kv;
     }
 
     public double update(double reference, double state) {
-        double error = reference - state;
-
-        double derivative = (error - lastError) / timer.seconds();
-        integralSum = integralSum + (error * timer.seconds());
-
-        return (Kp * error) + (Ki * integralSum) + (Kd * derivative) + (Kv * reference);
+        return super.update(reference, state) + (Kv * reference);
     }
 }
