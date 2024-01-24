de6008ff6fab223ed456ac0374245de8f43f413c
Harry Kruger <dsharryh27@gmail.com>
2024-01-24 14:52:29 +0200

    fix arm controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/utils/ArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/utils/ArmController.java
@@ -8,18 +8,15 @@ public class ArmController {
     private DcMotorEx motor = null;
     private final double MAX_DOWN_VELOCITY = -500;
     private final double MAX_POWER = 0.5; // For safety reasons
-    private final int VIRTUAL_STOP_POSITION = 80;
-    private final int VIRTUAL_STOP_THRESSHOLD = 5;
+    private final int VIRTUAL_STOP_UPPER_POSITION = 85;
+    private final int VIRTUAL_STOP_LOWER_POSITION = 75;
 
     private final PIDF velocityPIDF = new PIDF(0.0001, 0.0000, 0.000, 0.00045);
-    private final PID positionPID = new PID(0.01, 0.01
-            , 0);
+    private final PID positionPID = new PID(0.0005, 0.01
+            , 0.0001);
 
-    private boolean onVirtualStop = false;
     private boolean previousZeroing = false;
     private boolean zeroed = false;
-
-    public double slowDown = 0;
 
     public ArmController(DcMotorEx motor) {
         this.motor = motor;
@@ -33,15 +30,21 @@ public class ArmController {
     }
 
     public void updateArmState(double power, boolean virtualStop, boolean zeroing) {
-        double powerCommand = power;
-        if (zeroed && !zeroing && virtualStop) {
-            if (motor.getCurrentPosition() < VIRTUAL_STOP_POSITION) {
-                powerCommand = positionPID.update(VIRTUAL_STOP_POSITION, motor.getCurrentPosition()) + Math.max(powerCommand, 0);
-            } else if (Math.abs(motor.getCurrentPosition() - VIRTUAL_STOP_POSITION) < VIRTUAL_STOP_THRESSHOLD) {
-                powerCommand = Math.max(powerCommand, 0);
+        double powerCommand = 0;
+        if (zeroed && !zeroing && virtualStop && motor.getCurrentPosition() < VIRTUAL_STOP_UPPER_POSITION) {
+            powerCommand = Math.max(power, 0);
+            if (motor.getCurrentPosition() < VIRTUAL_STOP_LOWER_POSITION) {
+                powerCommand = positionPID.update(VIRTUAL_STOP_LOWER_POSITION, motor.getCurrentPosition()) + powerCommand;
+            } else {
+                motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             }
         } else {
-            if (powerCommand == 0) {
+            powerCommand = power;
+            if (!virtualStop) {
+                motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                if (zeroed && motor.getCurrentPosition() < 10)
+                    powerCommand = 0;
+            } else if (powerCommand == 0) {
                 motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
                 powerCommand = velocityPIDF.update(powerCommand, motor.getVelocity());
             } else if (motor.getVelocity() < MAX_DOWN_VELOCITY) {
