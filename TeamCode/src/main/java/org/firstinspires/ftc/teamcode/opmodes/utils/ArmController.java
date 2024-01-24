f409a37ecdbbf1a7c9789f4b8141b15b31ac211f
Harry Kruger <dsharryh27@gmail.com>
2024-01-24 14:11:30 +0200

    simplify arm controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/utils/ArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/utils/ArmController.java
@@ -6,19 +6,20 @@ import com.qualcomm.robotcore.util.Range;
 
 public class ArmController {
     private DcMotorEx motor = null;
-    private final int TARGET_VELOCITY = 800;
+    private final double MAX_DOWN_VELOCITY = -500;
     private final double MAX_POWER = 0.5; // For safety reasons
     private final int VIRTUAL_STOP_POSITION = 80;
     private final int VIRTUAL_STOP_THRESSHOLD = 5;
-    private final int VIRTUAL_STOP_SLOWDOWN = 100;
-    private final int VIRTUAL_STOP_SLOWDOWN_VELOCITY = 250;
 
-    private final PIDF velocityPIDF = new PIDF(0.0005, 0.0000, 0.000, 0.00045);
-    private final PID positionPID = new PID(0.00000, 0.02
-            , 0.0000); // TODO: implement motion profile
+    private final PIDF velocityPIDF = new PIDF(0.0001, 0.0000, 0.000, 0.00045);
+    private final PID positionPID = new PID(0.01, 0.01
+            , 0);
 
     private boolean onVirtualStop = false;
     private boolean previousZeroing = false;
+    private boolean zeroed = false;
+
+    public double slowDown = 0;
 
     public ArmController(DcMotorEx motor) {
         this.motor = motor;
@@ -31,48 +32,34 @@ public class ArmController {
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public void update(double power, boolean pushDown, boolean zeroing, boolean hangingOnRigging) {
-        if ((motor.getCurrentPosition() > VIRTUAL_STOP_POSITION + 10 && !onVirtualStop) || power > 0 || pushDown || hangingOnRigging) {
-            positionPID.reset();
-            onVirtualStop = false;
-            boolean inSlowdown = motor.getCurrentPosition() < VIRTUAL_STOP_POSITION + VIRTUAL_STOP_SLOWDOWN;
-            if (pushDown || (power != 0 && !inSlowdown)) {
-                motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            } else {
-                motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    public void updateArmState(double power, boolean virtualStop, boolean zeroing) {
+        double powerCommand = power;
+        if (zeroed && !zeroing && virtualStop) {
+            if (motor.getCurrentPosition() < VIRTUAL_STOP_POSITION) {
+                powerCommand = positionPID.update(VIRTUAL_STOP_POSITION, motor.getCurrentPosition()) + Math.max(powerCommand, 0);
+            } else if (Math.abs(motor.getCurrentPosition() - VIRTUAL_STOP_POSITION) < VIRTUAL_STOP_THRESSHOLD) {
+                powerCommand = Math.max(powerCommand, 0);
             }
-
-            if (pushDown && motor.getCurrentPosition() < 5) {
-                setPower(0);
-            } else {
-                double reference = power * TARGET_VELOCITY;
-                if (inSlowdown) reference = Math.max(reference, -VIRTUAL_STOP_SLOWDOWN_VELOCITY);
-                if (pushDown) reference = -100;
-
-                double powerCommand = velocityPIDF.update(reference, motor.getVelocity());
-                powerCommand = (reference < 0) ? Math.min(powerCommand, 0) : Math.max(powerCommand, 0);
-
-                if (hangingOnRigging) motor.setPower(powerCommand);
-                else setPower(powerCommand);
-            }
-
         } else {
-            onVirtualStop = true;
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            if (Math.abs(motor.getCurrentPosition() - VIRTUAL_STOP_POSITION) > VIRTUAL_STOP_THRESSHOLD)
-                setPower(positionPID.update(VIRTUAL_STOP_POSITION, motor.getCurrentPosition()));
-            else {
-                setPower(0);
+            if (powerCommand == 0) {
+                motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                powerCommand = velocityPIDF.update(powerCommand, motor.getVelocity());
+            } else if (motor.getVelocity() < MAX_DOWN_VELOCITY) {
+                motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                powerCommand = 0;
             }
         }
+
         if (previousZeroing && !zeroing) {
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            zeroed = true;
         }
         previousZeroing = zeroing;
+        setLimitedPower(powerCommand);
     }
 
-    void setPower(double power) {
+    void setLimitedPower(double power) {
         motor.setPower(Range.clip(power, -MAX_POWER, MAX_POWER));
     }
 }
