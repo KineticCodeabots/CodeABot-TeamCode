edfce857a0ec7680de660c028f9ea631ccba73d5
bluescorpian <dsharryh27@gmail.com>
2024-04-22 00:10:59 +0200

    add group to MecanumTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/MecanumTeleOp.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
+@TeleOp(group = "Testing")
 public class MecanumTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
