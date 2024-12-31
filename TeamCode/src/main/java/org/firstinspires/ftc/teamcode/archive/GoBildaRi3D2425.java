25422187b01e694ed9c8b32acff6b9e1a7be5616
bluescorpian <dsharryh27@gmail.com>
2024-12-31 13:39:32 +0200

    TeamCode organization and refactoring robot hardware into separate Robot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archive/GoBildaRi3D2425.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archive/GoBildaRi3D2425.java
@@ -20,7 +20,7 @@
  *   SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.archive;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
