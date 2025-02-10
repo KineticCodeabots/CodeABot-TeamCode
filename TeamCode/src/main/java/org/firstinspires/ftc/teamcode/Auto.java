8addca94e2163e7e31bccd6a3b28c3f6d6375228
bluescorpian <dsharryh27@gmail.com>
2025-02-10 20:37:25 +0200

    Add extra auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import androidx.annotation.NonNull;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
@@ -12,28 +10,34 @@ import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta;
 public class Auto extends LinearOpMode {
     public static int DRIVE_FORWARD_DISTANCE = 1600;
     public static int ARM_POSITION = 1070;
-    public static double DRiVE_SPEED = 0.2;
+    public static double DRIVE_SPEED = 0.3;
     public static int PARK_STRAFE_DISTANCE = 2000;
     public static int SPECIMEN_PARK_DISTANCE = 3000;
-    public static boolean DO_PARK = true;
 
-    public enum AutoMode {
+    public enum AutoStart {
         SPECIMEN,
-        PARK
+        NONE
     }
 
-    public AutoMode autoMode;
+    public enum AutoEnding {
+        PARK,
+        SAMPLE_PICKUP,
+    }
+
+    public AutoStart autoStart;
+    public AutoEnding autoEnding;
 
     private final Robot robot = new Robot(this);
     private final AutoRobot autoRobot = new AutoRobot(this, robot);
 
-    public Auto(AutoMode autoMode) {
-        this.autoMode = autoMode;
+    public Auto(AutoStart autoStart, AutoEnding autoEnding) {
+        this.autoStart = autoStart;
+        this.autoEnding = autoEnding;
     }
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Auto Mode", autoMode);
+        telemetry.addData("Auto Mode", autoStart);
         telemetry.update();
         robot.init();
         waitForStart();
@@ -41,25 +45,46 @@ public class Auto extends LinearOpMode {
         robot.claw.setPosition(Robot.CLAW_CLOSED_POSITION);
         sleep(1000);
 
-        if (autoMode == AutoMode.SPECIMEN) {
+        if (autoStart == AutoStart.SPECIMEN) {
             robot.armMotor.setTargetPosition(ARM_POSITION);
-            robot.armMotor.setPower(0.2);
-            robot.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            autoRobot.drive(DRIVE_FORWARD_DISTANCE, DRiVE_SPEED);
-            robot.armMotor.setTargetPosition(700);
             robot.armMotor.setPower(0.3);
-            autoRobot.drive((1000 - DRIVE_FORWARD_DISTANCE), DRiVE_SPEED);
+            robot.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            autoRobot.drive(DRIVE_FORWARD_DISTANCE, DRIVE_SPEED);
+            robot.armMotor.setTargetPosition(700);
+            robot.armMotor.setPower(0.4);
+            autoRobot.drive((1000 - DRIVE_FORWARD_DISTANCE), 0.2);
 
             robot.claw.setPosition(Robot.CLAW_OPEN_POSITION);
             robot.armMotor.setTargetPosition(100);
-            robot.armMotor.setPower(0.05);
-            autoRobot.drive(-700, DRiVE_SPEED);
-            if (DO_PARK) {
-                autoRobot.strafe(SPECIMEN_PARK_DISTANCE, DRiVE_SPEED);
+            robot.armMotor.setPower(0.1);
+            autoRobot.drive(-700, DRIVE_SPEED);
+
+
+        } else {
+            autoRobot.drive(200, DRIVE_SPEED);
+
+        }
+        if (autoEnding == AutoEnding.PARK) {
+            autoRobot.strafe(SPECIMEN_PARK_DISTANCE, DRIVE_SPEED);
+        } else if (autoEnding == AutoEnding.SAMPLE_PICKUP) {
+            autoRobot.strafe(2800, DRIVE_SPEED);
+            autoRobot.drive(900, DRIVE_SPEED);
+
+            robot.armMotor.setTargetPosition(100);
+            robot.armMotor.setPower(0.1);
+            robot.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.liftMotor.setTargetPosition(400);
+            robot.liftMotor.setPower(0.8);
+            robot.liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            while (robot.liftMotor.isBusy()) {
+
             }
-        } else if (autoMode == AutoMode.PARK) {
-            autoRobot.drive(200, DRiVE_SPEED);
-            autoRobot.strafe(PARK_STRAFE_DISTANCE, DRiVE_SPEED);
+            robot.claw.setPosition(Robot.CLAW_CLOSED_POSITION);
+            sleep(500);
+            robot.armMotor.setTargetPosition(200);
+            robot.armMotor.setPower(0.2);
+            sleep(200);
+            autoRobot.drive(-900, DRIVE_SPEED);
         }
 
 
@@ -71,14 +96,17 @@ public class Auto extends LinearOpMode {
 
     @OpModeRegistrar
     public static void register(OpModeManager manager) {
-        for (AutoMode autoMode : AutoMode.values()) {
-            OpModeMeta meta = new OpModeMeta.Builder()
-                    .setName(String.format("Auto - %s", autoMode))
-                    .setFlavor(OpModeMeta.Flavor.AUTONOMOUS)
-                    .setTransitionTarget("TeleOp")
-                    .build();
+        for (AutoStart autoStart : AutoStart.values()) {
+            for (AutoEnding autoEnding : AutoEnding.values()) {
+                OpModeMeta meta = new OpModeMeta.Builder()
+                        .setName(String.format("Auto - %s - %s", autoStart, autoEnding))
+                        .setFlavor(OpModeMeta.Flavor.AUTONOMOUS)
+                        .setTransitionTarget("TeleOp")
+                        .build();
 
-            manager.register(meta, new Auto(autoMode));
+                manager.register(meta, new Auto(autoStart, autoEnding));
+
+            }
         }
     }
 }
