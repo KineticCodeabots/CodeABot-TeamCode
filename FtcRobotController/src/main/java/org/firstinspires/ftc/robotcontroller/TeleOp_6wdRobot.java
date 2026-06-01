89f2a6cbdbbe87b1782b7ea8b882b8712c082a0d
JoelDickson <150940541+KineticCodeabots@users.noreply.github.com>
2026-06-01 17:29:12 +0200

    New drivetrain and intake code for 6wd robot
    Simple code for a 6wd robot that the rookies of the team have been building to learn the basics of building

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TeleOp_6wdRobot.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.robotcontroller;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class TeleOp_6wdRobot extends OpMode {
+    BackRoundCode6wdRobot drive = new BackRoundCode6wdRobot();
+    double throttle, spin, intakePower;
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap); //initialises motor from Back_round_Code_6wd
+    }
+
+    @Override
+    public void loop() { //sets throttle and spin to the according gamepad sticks and sets the intakes control to right trigger
+        throttle = gamepad1.left_stick_y;
+        spin = -gamepad1.right_stick_x;
+        intakePower = gamepad1.right_trigger;
+
+        drive.drive(throttle, spin, intakePower);
+
+    }
+}
